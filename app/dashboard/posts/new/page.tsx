ab45788814a5101fde0fac36e0761c19ec8e2139
Aaron Kili <aaronkili@Mac.lan>
2025-07-01 00:34:42 -0500

    Remove old dashboard files causing build error

--- app/dashboard/posts/new/page.tsx
+++
@@ -1,9 +0,0 @@
-import PostCreator from '@/app/components/PostCreator'
-
-export default function NewPostPage() {
-  return (
-    <div className="min-h-screen bg-gray-50 py-8">
-      <PostCreator />
-    </div>
-  )
-} 
